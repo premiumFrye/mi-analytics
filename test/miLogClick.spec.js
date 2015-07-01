53920f1690b7aaf476209eac7e54bfa77bd5f1c8
Nicholas Frye <nick@mobileigniter.com>
2015-07-01 16:46:57 -0500

    unit tests for event names & payload with miLogData

--- test/miLogClick.spec.js
+++ test/miLogClick.spec.js
@@ -1,20 +1,27 @@
 /*global describe, beforeEach, it, inject, expect*/
 describe('miLogClick', function () {
-  var logResult;
+  var logDetails,
+    logName;
   beforeEach(module('miAnalytics'));
   beforeEach(inject(function ($compile, $rootScope, miLogger) {
     var scope = $rootScope.$new(),
       element = angular.element("<div mi-log-click=\"test-event\" mi-log-data=\"{'data': 'test'}\">I'm a test!</div>'"),
       logAction = function (name, object) {
-        logResult = {'event': name, 'payload': object};
+        logName = name;
+        logDetails = {'event': name, 'payload': object};
       };
     $compile(element)(scope);
     miLogger.setLogAction(logAction);
     element.triggerHandler('click');
 
   }));
-
-  it('should log click events', function () {
-    expect(logResult).to.eql({'event': 'click', 'payload': {'description': 'test-event', 'details' : {'data': 'test'}}});
+  it('sends click event', function () {
+    expect(logName).to.eql('click');
+  });
+  it('send event name', function () {
+    expect(logDetails.payload.description).to.eql('test-event');
+  });
+  it('send details in mi-log-data', function () {
+    expect(logDetails).to.eql({'event': 'click', 'payload': {'description': 'test-event', 'details' : {'data': 'test'}}});
   });
 });
