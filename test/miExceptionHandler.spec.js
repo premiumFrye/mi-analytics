3130bf1554b6f9b225f527041db9d2fc500a7822
Nicholas Frye <nick@mobileigniter.com>
2015-07-02 13:06:11 -0500

    tests for exception handler

--- test/miExceptionHandler.spec.js
+++ test/miExceptionHandler.spec.js
@@ -1,25 +1,35 @@
 /*global describe, beforeEach, it, inject, expect*/
-describe("ExampleCtrl", function () {
+describe("exception handler", function () {
   var errName,
     errPayload,
-    retarded = new Error("test error");
+    errCause = "user",
+    errMessage = "test error",
+    errTest = new Error(errMessage);
   beforeEach(module('miAnalytics'));
   beforeEach(inject(function (miLogger) {
     var testLogAction = function (name, payload) {
       errName = name;
       errPayload = payload;
     };
+    // console.log(Object.keys(errTest), errPayload);
     miLogger.setLogAction(testLogAction);
   }));
   it("should throw an error", inject(function ($exceptionHandler) {
     expect(function () {
-      $exceptionHandler(retarded, 'user');
-    }).to.throw(retarded);
+      $exceptionHandler(errTest, errCause);
+    }).to.throw(errTest);
   }));
-  it("should log the error", function () {
+  it("should log an error event", function () {
     expect(errName).to.equal('error');
   });
-  it("should log the entire error", function () {
-    console.log(errPayload, retarded, errPayload == retarded, errPayload === retarded);
+  it("should log the entire error object", function () {
+    expect(errPayload).to.include.keys('details');
+    expect(errPayload).to.include.keys('description');
+  });
+  it("should include the error message", function () {
+    expect(errPayload.details.message).to.include(errMessage);
+  });
+  it("should include the error cause", function () {
+    expect(errPayload.details.message).to.include(errCause);
   });
 });
